8bbdd37dd7ea4472b4f65321cd89b3408f3f2b65
laura <lauradondiego@gmail.com>
2019-12-04 13:48:58 -0500

    created a class called Room in room.py file

--- src/player.py
+++ src/player.py
@@ -1,2 +1,4 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+##this could be like 4 lines
--- src/room.py
+++ src/room.py
@@ -1,2 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    name = ""
+    description = ""
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f" Room-Name: {self.name} Room-Description: {self.description}"
+
+
+obj = Room("Kitchen", "There's something in the dishwasher")
+print("ROOM:", obj)
