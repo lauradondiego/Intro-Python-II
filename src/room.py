d3ee4a48362c4336ac5d38b246548b34eab463d9
laura <lauradondiego@gmail.com>
2019-12-04 15:23:20 -0500

    added directions to Room class

--- src/room.py
+++ src/room.py
@@ -9,10 +9,19 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
 
     def __str__(self):
-        return f" Room-Name: {self.name} Room-Description: {self.description}"
+        return f" Room-Name: {self.name}, Description: {self.description}, Directions: N: {self.n_to}, S: {self.s_to}, E: {self.e_to}, W: {self.w_to}"
 
 
-obj = Room("Kitchen", "There's something in the dishwasher")
+obj = Room("testFoyer", "testDescription")
 print("ROOM:", obj)
+
+
+# The room should have name and description attributes.
+# The room should also have n_to, s_to, e_to, and w_to
+# attributes which point to the room in that respective direction.
