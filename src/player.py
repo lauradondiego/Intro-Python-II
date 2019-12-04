348a3c37d66d107dd999bd007b8ae03ed14c22d3
laura <lauradondiego@gmail.com>
2019-12-04 14:00:16 -0500

    created a class called Player in player.py file

--- src/player.py
+++ src/player.py
@@ -1,4 +1,18 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
-##this could be like 4 lines
+
+class Player:
+    currentRoom = ""
+    # should this have the super() constructor to inherit all
+    # properties of Room class?? (like: name, description)
+
+    def __init__(self, currentRoom):
+        self.currentRoom = currentRoom
+
+    def __str__(self):
+        return f"Current-Room: {self.currentRoom}"
+
+
+obj = Player("Kitchen")
+print("PLAYER INFO:", obj)
